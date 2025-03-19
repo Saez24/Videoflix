9a8e1543a8fe31e83962af4b637378e359dd81c0
Saez24 <saez0724+github@googlemail.com>
2025-03-19 18:31:44 +0100

    working on convert to hls

--- content/tasks.py
+++ content/tasks.py
@@ -17,23 +17,16 @@ def exportJson():
         f.write(dataset.json)
     EOF
     """
-    subprocess.run(cmd, shell=True, text=True)
-
-    
-
-# def convert720p(source):
-#     new_file_name = source[:-4] + '_720p.mp4' # Alter name + _720p.mp4
-#     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-#     subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, text=True, check=True)
 
 def create_base_directory(source):
     base_name = os.path.join(settings.MEDIA_ROOT, 'videos', 'hls', os.path.basename(source).rsplit('.', 1)[0])
     os.makedirs(base_name, exist_ok=True)
     return base_name
 
-
 def generate_ffmpeg_command(source, base_name, quality, resolution, bitrate):
     print(f'Generating command for {quality}')
+
     return [
         'ffmpeg',
         '-i', source,
@@ -46,14 +39,13 @@ def generate_ffmpeg_command(source, base_name, quality, resolution, bitrate):
         '-c:v', 'libx264',
         '-b:v', bitrate,
         '-c:a', 'aac',
-        '-strict', '-2',
+        '-b:a', '128k',  # Falls Audio vorhanden ist
         '-f', 'hls',
         '-hls_time', '10',
         '-hls_playlist_type', 'vod',
-        '-hls_segment_filename', f'{base_name}/{quality}_%03d.ts',
-        f'{base_name}/{quality}.m3u8'
+        '-hls_segment_filename', os.path.join(base_name, f'{quality}_%03d.ts'),
+        os.path.join(base_name, f'{quality}.m3u8')
     ]
-
 
 def convert_to_hls(source, video_id):
     """
@@ -64,4 +56,8 @@ def convert_to_hls(source, video_id):
     
     for quality, (resolution, bitrate) in QUALITIES.items():
         cmd = generate_ffmpeg_command(source, base_name, quality, resolution, bitrate)
-        subprocess.run(cmd)    
+        try:
+            subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(f"✅ Conversion for {quality} completed.")
+        except subprocess.CalledProcessError as e:
+            print(f"❌ Error converting {quality}: {e.stderr.decode()}")
