71b030237393dfbb961e5f8ac4cc98ca0f8d0a24
Saez24 <saez0724+github@googlemail.com>
2025-03-21 07:22:51 +0100

    changes on tasks.py

--- content/signals.py
+++ content/signals.py
@@ -1,42 +1,32 @@
 import os
-from django.dispatch import receiver
-
-from content.tasks import convert, delete_video_folder, delete_thumbnail_folder
-from .models import Video
 from django.db.models.signals import post_save, post_delete
+from content.models import Video
+from django.dispatch import receiver
 import django_rq
+from .tasks import convert_to_hls, delete_video_folder, delete_thumbnail_folder
 
+print('Signals loaded')
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     """
-    This function queues the creation of the HLS files to a rq-worker.
+    Sends variables to tasks.py to format through RQ Worker
     """
     if created:
-        queue = django_rq.get_queue("default", autocommit=True)
-        queue.enqueue(
-            convert,
-            instance.video_file.path,
-            instance.title,
-        )
-
+        queue = django_rq.get_queue('default', autocommit=True)
+        queue.enqueue(convert_to_hls, instance.video_file.path, instance.id, job_timeout=360, result_ttl=0)
 
 @receiver(post_delete, sender=Video)
-def video_post_delete(sender, instance, **kwargs):
-    """
-    This starts the deletion of the video folder.
-    """
-    if instance.video_file:
-        base_folder = os.path.join("media", "videos", instance.title.replace(" ", "_"))
-        if os.path.exists(base_folder):
-            delete_video_folder(base_folder)
-
-
-@receiver(post_delete, sender=Video)
-def thumbnail_post_delete(sender, instance, **kwargs):
-    """
-    This function starts the deletion of the thumbnail.
-    """
-    if instance.thumbnail:
-        thumbnail_folder = os.path.dirname(instance.thumbnail.path)
-        delete_thumbnail_folder(thumbnail_folder)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+        print("auto_delete_file_on_delete triggered")
+        if instance.video_file:
+            print(f"Video file path: {instance.video_file.path}")
+            if os.path.isfile(instance.video_file.path):
+                os.remove(instance.video_file.path)
+                print("Video file deleted")
+                
+        if instance.thumbnail:
+            print(f"Thumbnail path: {instance.thumbnail.path}")
+            if os.path.isfile(instance.thumbnail.path):
+                os.remove(instance.thumbnail.path)
+                print("Thumbnail deleted")
