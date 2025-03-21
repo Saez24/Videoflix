56964b4960611039246ead72753b7fe52660bafc
Saez24 <saez0724+github@googlemail.com>
2025-03-21 07:02:53 +0100

    signals and tasks changes

--- content/signals.py
+++ content/signals.py
@@ -1,31 +1,42 @@
 import os
-from django.db.models.signals import post_save, post_delete
-from content.models import Video
 from django.dispatch import receiver
-import django_rq
-from .tasks import convert_to_hls
 
-print('Signals loaded')
+from content.tasks import convert, delete_video_folder, delete_thumbnail_folder
+from .models import Video
+from django.db.models.signals import post_save, post_delete
+import django_rq
+
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     """
-    Sends variables to tasks.py to format through RQ Worker
+    This function queues the creation of the HLS files to a rq-worker.
     """
     if created:
-        queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue(convert_to_hls, instance.video_file.path, instance.id, job_timeout=60, result_ttl=0)
+        queue = django_rq.get_queue("default", autocommit=True)
+        queue.enqueue(
+            convert,
+            instance.video_file.path,
+            instance.title,
+        )
+
 
 @receiver(post_delete, sender=Video)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-        print("auto_delete_file_on_delete triggered")
-        if instance.video_file:
-            print(f"Video file path: {instance.video_file.path}")
-            if os.path.isfile(instance.video_file.path):
-                os.remove(instance.video_file.path)
-                print("Video file deleted")
-        if instance.thumbnail:
-            print(f"Thumbnail path: {instance.thumbnail.path}")
-            if os.path.isfile(instance.thumbnail.path):
-                os.remove(instance.thumbnail.path)
-                print("Thumbnail deleted")
+def video_post_delete(sender, instance, **kwargs):
+    """
+    This starts the deletion of the video folder.
+    """
+    if instance.video_file:
+        base_folder = os.path.join("media", "videos", instance.title.replace(" ", "_"))
+        if os.path.exists(base_folder):
+            delete_video_folder(base_folder)
+
+
+@receiver(post_delete, sender=Video)
+def thumbnail_post_delete(sender, instance, **kwargs):
+    """
+    This function starts the deletion of the thumbnail.
+    """
+    if instance.thumbnail:
+        thumbnail_folder = os.path.dirname(instance.thumbnail.path)
+        delete_thumbnail_folder(thumbnail_folder)
--- content/tasks.py
+++ content/tasks.py
@@ -1,90 +1,137 @@
-import os
 import subprocess
-from django.conf import settings
+import os
+import shutil
+from django.db import transaction
 from content.models import Video
 
-QUALITIES = {
-    '1080p': ('1920x1080', '5000k'),
-    '720p': ('1280x720', '2800k'),
-    '480p': ('854x480', '1400k')
-}
 
-# def exportJson():
-#     shell = [
-#         'python3', 'manage.py', 'shell'
-#     ]
-#     imp = [
-#         'from', 'core.admin', 'import', 'VideoResource'
-#     ]
-#     data = [
-#         'dataset', '=', 'VideoResource().export()'
-#     ]
-#     action = [
-#         'print(dataset.json)'
-#         'dataset.json', '>', 'exportJson.txt'
-#     ]
-#     subprocess.run(shell)
-#     subprocess.run(imp)
-#     subprocess.run(data)
-#     subprocess.run(action)
+def convert(source, folder_path):
+    """
+    This function converts a video file into the desired HLS files. It is called as a signal so it can run in the background.
+    """
+    sanitized_folder_path = folder_path.replace(" ", "_")
+    base_name = os.path.splitext(os.path.basename(source))[0]
+    output_dir = create_output_directory(sanitized_folder_path)
 
-    
+    resolutions = {
+        "360p": ("hls_file_360", "640x360"),
+        "480p": ("hls_file_480", "854x480"),
+        "720p": ("hls_file_720", "1280x720"),
+        "1080p": ("hls_file_1080", "1920x1080"),
+    }
 
-# def convert720p(source):
-#     new_file_name = source[:-4] + '_720p.mp4' # Alter name + _720p.mp4
-#     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-#     subprocess.run(cmd, capture_output=True)
+    with transaction.atomic():
+        video_instance = Video.objects.get(title=folder_path)
 
-def create_base_directory(source):
-    base_name = os.path.join(settings.MEDIA_ROOT, 'videos', 'hls', os.path.basename(source).rsplit('.', 1)[0])
-    os.makedirs(base_name, exist_ok=True)
-    return base_name
+        for suffix, (field_name, resolution) in resolutions.items():
+            process_resolution(
+                video_instance,
+                source,
+                base_name,
+                output_dir,
+                suffix,
+                field_name,
+                resolution,
+            )
+
+        video_instance.save()
 
 
-def generate_ffmpeg_command(source, base_name, quality, resolution, bitrate):
+def create_output_directory(sanitized_folder_path):
+    """
+    This function creates the output directory for the HLS files based on the name of the video file.
+    """
+    output_dir = os.path.join("media", "videos", sanitized_folder_path, "HLS_files")
+    os.makedirs(output_dir, exist_ok=True)
+    return output_dir
 
-    return [
-        'ffmpeg',
-        '-i', source,
-        '-preset', 'fast',
-        '-g', '48',
-        '-sc_threshold', '0',
-        '-map', '0:v',
-        '-map', '0:a?',
-        '-s:v', resolution,
-        '-c:v', 'libx264',
-        '-b:v', bitrate,
-        '-c:a', 'aac',
-        '-strict', '-2',
-        '-f', 'hls',
-        '-hls_time', '5',
-        '-hls_playlist_type', 'vod',
-        '-hls_segment_filename', f'{base_name}/{quality}_%03d.ts',
-        f'{base_name}/{quality}.m3u8'
+
+def process_resolution(
+    video_instance, source, base_name, output_dir, suffix, field_name, resolution
+):
+    """
+    This function starts the process of starts the creation of the the HLS files. In the end it deletes the helper mp4 file.
+    """
+    resolution_file = os.path.join(output_dir, f"{base_name}_{suffix}.mp4")
+    convert_to_resolution(source, resolution_file, resolution)
+
+    hls_prefix = os.path.join(output_dir, f"{base_name}_{suffix}")
+    m3u8_path = convert_to_hls(resolution_file, hls_prefix)
+    setattr(video_instance, field_name, m3u8_path.replace("media/", ""))
+
+    delete_mp4(resolution_file)
+
+
+def convert_to_resolution(source, output_name, resolution):
+    """
+    This function uses the FFmpeg tool to convert a video file to a specified resolution.
+    """
+    cmd = [
+        "/usr/bin/ffmpeg",
+        "-i",
+        source,
+        "-s",
+        resolution,
+        "-c:v",
+        "libx264",
+        "-crf",
+        "23",
+        "-c:a",
+        "aac",
+        "-strict",
+        "-2",
+        output_name,
     ]
-  
+    subprocess.run(cmd, check=True)
 
 
-def convert_to_hls(source, video_id):
+def convert_to_hls(source, output_name_prefix):
     """
-    Converts source file to HLS with multiple quality levels and saves files in corresponding folder.
+    This function converts an input file to a specified HLS file.
     """
-    print(f'Converting {source} to HLS')
-    base_name = create_base_directory(source)
-    
-    try:
-        for quality, (resolution, bitrate) in QUALITIES.items():
-            cmd = generate_ffmpeg_command(source, base_name, quality, resolution, bitrate)
-            print(f'Running command: {" ".join(cmd)}')
-            
-            subprocess.run(cmd, capture_output=True)
+    m3u8_file = f"{output_name_prefix}.m3u8"
+    segment_pattern = f"{output_name_prefix}_%03d.ts"
+    cmd = [
+        "/usr/bin/ffmpeg",
+        "-i",
+        source,
+        "-codec",
+        "copy",
+        "-start_number",
+        "0",
+        "-hls_time",
+        "10",
+        "-hls_list_size",
+        "0",
+        "-f",
+        "hls",
+        "-hls_segment_filename",
+        segment_pattern,
+        m3u8_file,
+    ]
+    subprocess.run(cmd, check=True)
+    return m3u8_file
 
-            print(f'Command executed for {quality}')
 
-            video = Video.objects.get(id=video_id)
-            video.hls_playlist = os.path.join(base_name, 'playlist.m3u8')
-            video.save() 
+def delete_mp4(resolution_file):
+    """
+    This function deletes the mp4 file that is created for creating the HLS files.
+    """
+    if os.path.exists(resolution_file):
+        os.remove(resolution_file)
 
-    except Exception as e:
-        print(f'Error saving HLS playlist: {str(e)}')
-        raise
+
+def delete_video_folder(folder_path):
+    """
+    This function deletes the video folder if a user deletes a video in the admin panel.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+
+
+def delete_thumbnail_folder(folder_path):
+    """
+    This function deletes the thumbnail folder if a user deletes a video in the admin panel.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
