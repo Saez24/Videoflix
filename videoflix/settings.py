95e539069a56a53f71019d9e67dc3a439a2e597c
Saez24 <saez0724+github@googlemail.com>
2025-04-27 18:39:10 +0200

    change debug to false

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -43,7 +43,7 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@videoflix.com
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
